b6b3e88b8a5d5f5af1deed099a270dbbb12560cc
Dtry03 <xhuss03@gmail.com>
2026-09-01 22:41:50 +0200

    last design changes (footer creation, clients-photos &  ISOs icons)

--- app/footer.tsx
+++ app/footer.tsx
@@ -10,21 +10,26 @@ const Footer = () => {
             
         <footer className="footer flex flex-col justify-center items-center md:flex-row bg-episrubi-accent p-5 gap-5">
 
-          
-
-
-       
-
-            <div className="slogan-container flex flex-col justify-center items-center">
+            <div className="information-container item-hidden  flex flex-col justify-center items-center">
 
                 <h3 className="text-center font-bespoke-800 md:text-2xl text-xl uppercase italic">EPIS RUBÍ. Viste tu trabajo, protege tu futuro </h3>
 
 
-                <ul className="contact-list flex flex-col md:flex-row text-center md:gap-5">
+                <ul className="font-bespoke-400 contact-list flex flex-col md:flex-row text-center md:gap-5">
 
                     <li>Teléfono: 646 851 944</li>
+                    <li>Email: episrubi@episrubi</li>
                     <li>Dirección: Avenida Estatut 28 Rubí</li>
                 </ul>
+
+                 <ul className="font-bespoke-400 schedule-list flex flex-col md:flex-row text-center md:gap-5">
+
+                    <li>Horario</li>
+                    <li>9:00 - 13:00</li>
+                    <li>15:00 - 19:00</li>
+                   
+                </ul>
+
 
 
             </div>
